ddafabf70508dceba1ea23968f04344814d56cd0
DanielaMendezAr <danielamendez0809@gmail.com>
2023-10-02 14:46:50 -0600

    finalización

--- src/Views/header.php
+++ src/Views/header.php
@@ -10,18 +10,22 @@
         <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarText">
             <ul class="navbar-nav ml-auto d-none d-sm-flex">
                 <li class="nav-item">
-                    <a class="nav-link active" href="https://www.gob.mx/gobierno" target="_blank" alt="Gobierno">Gobierno</a>
+                    <a class="nav-link active" href="https://www.gob.mx/gobierno" target="_blank"
+                        alt="Gobierno">Gobierno</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="https://www.participa.gob.mx/" target="_blank" alt="Participa">Participa</a>
+                    <a class="nav-link active" href="https://www.participa.gob.mx/" target="_blank"
+                        alt="Participa">Participa</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" href="https://datos.gob.mx/" target="_blank" alt="Datos">Datos</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" href="https://www.gob.mx/busqueda" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-search" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                         </svg>
                     </a>
                 </li>
@@ -76,14 +80,18 @@
         </div>
         <div class="col-sm-auto justify-content-end d-none d-xl-block">
             <div class="text-right">
-                <a href="https://globalpage-prod.webex.com/join?surl=https%3A%2F%2Fsignin.webex.com%2Fcollabs%2F%23%2Fmeetings%2Fjoinbynumber%3FTrackID%3D%26hbxref%3D%26goid%3Dattend-meeting" title="Microsoft Teams" target="_blank" style="text-decoration: none;">
+                <a href="https://globalpage-prod.webex.com/join?surl=https%3A%2F%2Fsignin.webex.com%2Fcollabs%2F%23%2Fmeetings%2Fjoinbynumber%3FTrackID%3D%26hbxref%3D%26goid%3Dattend-meeting"
+                    title="Microsoft Teams" target="_blank" style="text-decoration: none;">
                     <img loading='lazy' height="40" src="img/encabezado/microsoft_teams.png" alt="MT">
                 </a>
-                <a href="https://login.microsoftonline.com/?whr=tecnm.mx" title="Correo Institucional" target="_blank" style="text-decoration: none;">
+                <a href="https://login.microsoftonline.com/?whr=tecnm.mx" title="Correo Institucional" target="_blank"
+                    style="text-decoration: none;">
                     <img loading='lazy' height="40" src="img/encabezado/correo-icono.png" alt="Buzón">
                 </a>
-                <a href="pdf/Calendario_Academico_2019-2020.pdf" title="Calendario Académico" target="_blank" style="text-decoration: none;">
-                    <img loading='lazy' height="40" src="img/encabezado/calendario-icono.png" alt="Calendario Académico">
+                <a href="pdf/Calendario_Academico_2019-2020.pdf" title="Calendario Académico" target="_blank"
+                    style="text-decoration: none;">
+                    <img loading='lazy' height="40" src="img/encabezado/calendario-icono.png"
+                        alt="Calendario Académico">
                 </a>
 
                 <!-- Traductor -->
@@ -103,15 +111,9 @@
                     <img height="40" src='img/encabezado/corea-icono.png' title='Español' alt='Idioma Español'>
                 </a>
 
-                <img loading='lazy' height="40" title="Accesibilidad" src="img/encabezado/ojo-icono.png" alt="Débil Visual" />
+                <img loading='lazy' height="40" title="Accesibilidad" src="img/encabezado/ojo-icono.png"
+                    alt="Débil Visual" />
 
-                <a class="btn--accesibilidad contraste" height="40">
-                    <svg class="svg-inline--fa fa-adjust fa-w-16" height="40" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="adjust" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                        <path fill="#1B396A" d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z">
-                            <title>Contraste</title>
-                        </path>
-                    </svg>
-                </a>
                 <button class="btnCambioTexto" onclick="return cambiarTexto('+')">A+</button>
                 <button class="btnCambioTexto" onclick="return cambiarTexto('-')">A-</button>
                 <button class="btnCambioTexto" onclick="resetLetra()" title="Restablecer tamaño">A</button>
@@ -123,7 +125,8 @@
     <nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #1B396A;">
         <div class="container-fluid">
             <!-- Botón de navegación para pantallas SM y MD -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!-- Botones de navegación -->
@@ -154,18 +157,29 @@
                         <a class="nav-link active hover-blue" href="?option=7">Contacto</a>
                     </li>
                     <li class="nav-item hover-blue dropdown">
-                        <a class="nav-link active dropdown-toggle hover-blue" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link active dropdown-toggle hover-blue" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             Enlaces
                         </a>
                         <ul class="dropdown-menu" style="background-color: #312e81;" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="https://www.tecnm.mx/">TECNM</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="http://www.veracruz.tecnm.mx/">ITVER</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="https://sii.veracruz.tecnm.mx/">SII</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="https://elearning.veracruz.tecnm.mx/">Moodle</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="http://www.veracruz.tecnm.mx/index.php/mapa-del-tecnm-veracruz">Mapa virtual 3D del ITVER</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="https://elibro.net/es/lc/itver/login_usuario/">Biblioteca eLibro</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="https://www.veracruz.tecnm.mx/index.php/normateca/documentos-operativos/manuales">Manuales del ITVER</a></li>
-                            <li><a class="dropdown-item hover-blue" target="_blank" href="https://bit.ly/3KVYAol">Reglamento de Estudiantes del TecNM</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="https://www.tecnm.mx/">TECNM</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="http://www.veracruz.tecnm.mx/">ITVER</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="https://sii.veracruz.tecnm.mx/">SII</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="https://elearning.veracruz.tecnm.mx/">Moodle</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="http://www.veracruz.tecnm.mx/index.php/mapa-del-tecnm-veracruz">Mapa virtual
+                                    3D del ITVER</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="https://elibro.net/es/lc/itver/login_usuario/">Biblioteca eLibro</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="https://www.veracruz.tecnm.mx/index.php/normateca/documentos-operativos/manuales">Manuales
+                                    del ITVER</a></li>
+                            <li><a class="dropdown-item hover-blue" target="_blank"
+                                    href="https://bit.ly/3KVYAol">Reglamento de Estudiantes del TecNM</a></li>
                         </ul>
                     </li>
                 </ul>
